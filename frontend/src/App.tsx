212d5551e708fbee8dcc021575875c27210fb85d
Hieunguyen <hieunvhe182603@fpt.edu.vn>
2025-06-13 12:02:59 +0700

    fix lỗi

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,54 +1,16 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./contexts/AuthContext";
-import { ProtectedRoute } from "./components/ProtectedRoute";
-import ProductDetail from "./components/product_manager/product_detail";
-import ProductList from "./components/product_manager/product_list";
-import UserList from "./components/user_manager/user_list";
-import Login from "./components/Login/Login.jsx";
-import CartPage from "./Page/CartPage.jsx";
-import CheckoutPage from "./Page/CheckoutPage.jsx";
-import OrderHistoryPage from "./Page/OrderHistoryPage.jsx";
+import ContactUs from "./components/About/ContactUs.jsx";
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          
-          <Route path="/login" element={<Login />} />
-          <Route path="/products" element={<ProductList />} />
-          <Route path="/products/:id" element={<ProductDetail />} />
-
-          
-          <Route path="/userList" element={
-            <ProtectedRoute>
-              <UserList />
-            </ProtectedRoute>
-          } />
-          <Route path="/cart" element={
-            <ProtectedRoute>
-              <CartPage />
-            </ProtectedRoute>
-          } />
-          <Route path="/checkout" element={
-            <ProtectedRoute>
-              <CheckoutPage />
-            </ProtectedRoute>
-          } />
-          <Route path="/order-history" element={
-            <ProtectedRoute>
-              <OrderHistoryPage />
-            </ProtectedRoute>
-          } />
-          <Route path="/products/add" element={
-            <ProtectedRoute>
-              <ProductDetail />
-            </ProtectedRoute>
-          } />
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<ContactUs />} />
+        <Route path="/contact" element={<ContactUs />} />
+      </Routes>
+    </Router>
   );
 }
 
