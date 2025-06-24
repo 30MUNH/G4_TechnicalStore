66fedd8a52ffc4d7e3baebc9694e774f731eba46
tungtn <tungtnhe140085@fpt.edu.vn>
2025-06-24 21:48:54 +0700

    refactor categories

--- backend/src/product/categories/category.entity.ts
+++ backend/src/product/categories/category.entity.ts
@@ -1,12 +1,8 @@
 // src/product/category.entity.ts
 
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { NamedEntity } from '@/common/NamedEntity';
+import { Entity } from 'typeorm';
 
-@Entity()
-export class Category {
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @Column({ unique: true })
-  name: string;
+@Entity('categories')
+export class Category extends NamedEntity{
 }
