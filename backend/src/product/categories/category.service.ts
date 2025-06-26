8247426745f1fcd0b9594c8f480b33cd863f92ae
Ambrose <manhndthe181128@fpt.edu.vn>
2025-06-27 00:34:06 +0700

    Fix bug

--- backend/src/product/categories/category.service.ts
+++ backend/src/product/categories/category.service.ts
@@ -1,24 +1,76 @@
 import { Service } from "typedi";
 import { Category } from "./category.entity";
-import { Repository, getRepository } from "typeorm";
+import { Repository } from "typeorm";
+import { DbConnection } from "@/database/dbConnection";
+import { CreateCategoryDto, UpdateCategoryDto } from "../dtos/category.dto";
 
 @Service()
 export class CategoryService {
   private categoryRepository: Repository<Category>;
 
   constructor() {
-    this.categoryRepository = getRepository(Category);
+    this.initializeRepository();
+  }
+
+  private async initializeRepository() {
+    const dataSource = await DbConnection.getConnection();
+    if (!dataSource) {
+      throw new Error("Database connection not available");
+    }
+    this.categoryRepository = dataSource.getRepository(Category);
   }
 
   async getAllCategories(): Promise<Category[]> {
+    await this.ensureRepository();
     return await this.categoryRepository.find({
       order: { name: "ASC" }
     });
   }
 
   async getCategoryBySlug(slug: string): Promise<Category | null> {
+    await this.ensureRepository();
     return await this.categoryRepository.findOne({
       where: { slug }
     });
+  }
+
+  async createCategory(createCategoryDto: CreateCategoryDto): Promise<Category> {
+    await this.ensureRepository();
+    const category = this.categoryRepository.create(createCategoryDto);
+    return await this.categoryRepository.save(category);
+  }
+
+  async updateCategory(id: string, updateCategoryDto: UpdateCategoryDto): Promise<Category | null> {
+    await this.ensureRepository();
+    const category = await this.categoryRepository.findOne({
+      where: { id }
+    });
+
+    if (!category) {
+      return null;
+    }
+
+    Object.assign(category, updateCategoryDto);
+    return await this.categoryRepository.save(category);
+  }
+
+  async deleteCategory(id: string): Promise<boolean> {
+    await this.ensureRepository();
+    const category = await this.categoryRepository.findOne({
+      where: { id }
+    });
+
+    if (!category) {
+      return false;
+    }
+
+    await this.categoryRepository.remove(category);
+    return true;
+  }
+
+  private async ensureRepository() {
+    if (!this.categoryRepository) {
+      await this.initializeRepository();
+    }
   }
 }
